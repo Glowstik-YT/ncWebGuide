2537a16c03937ad54796c5c9b7ec11eff1fbce82
TheUntraceable <ridhwanplayzzz@gmail.com>
2021-12-28 21:19:23 +0000

    Add a close warning button

--- pages/warning.js
+++ pages/warning.js
@@ -1,6 +1,12 @@
 import styles from "../styles/Info.module.css";
+import { useState } from "react"
 
 export default function CreateWarning({title, content}){
+    const [visible, setVisible] = useState(true);
+    function hideMe(){
+        setVisible(false);
+    }
+    
     return(
         <div>
             <div className={styles.warnCard}>
@@ -17,6 +23,7 @@ export default function CreateWarning({title, content}){
                         {content}
                     </p>
                 </div>
+                <button className={styles.closeTipButton} onClick={hideMe}>Close Warning</button>
                 </div>
             </div>
         </div>
