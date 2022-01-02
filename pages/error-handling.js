625e1990db777ca5782cc01bb841c2ee631b91c7
Glowstikk <69128866+Glowstik-YT@users.noreply.github.com>
2022-01-02 14:53:11 -0500

    added 3 examples to errorhandlingjs

--- pages/error-handling.js
+++ pages/error-handling.js
@@ -6,6 +6,24 @@ import CodeStyles from "../styles/CodeBlock.module.css";
 import info from "../styles/Info.module.css";
 import CreateWarning from "./warning";
 import Link from "next/link"
+
+const globalErrorExample = `@bot.event
+async def on_command_error(ctx: commands.Context, error: commands.CommandError):
+`
+
+const commandNotFoundExample = `if isinstance(error, commands.CommandNotFound):
+    return 
+#it is not recommended to have a message for everytime a command is not found
+`
+
+const commandOnCooldownExample = `elif isinstance(error, commands.MissingPermissions):
+    await ctx.send(f"You are on cooldown for {round(error.retry_after, 1)} seconds")
+`
+
+const missingPermissionsExample = `elif isinstance(error, commands.MissingPermissions):
+    await ctx.send(f"You are missing the following permissions: {error.missing_perms}")
+`
+
 export default function Cogs() {
     return (
         <div>
@@ -21,7 +39,33 @@ export default function Cogs() {
                 <h1>Error Handling</h1>
                 <p>Being able to handle errors is a big part of creating a discord bot, it ensures your bot will still work even if not everything goes to plan. Thats why nextcord has two ways to handle errors, per-command error handling and global error handling. Global error handling is a lot more popular as it is automatically applied to every command and is a quick way to do it. Although if you need specific error handling for certain commands, per-command error handling is probably the better option.</p>
                 <h3>Global Error Handling</h3>
-                
+                <p>They way we do this, is by creating an event with the name of <code className={CodeStyles.inline}>on_command_error</code>, we pass in <code className={CodeStyles.inline}>ctx</code> and the error itself.</p>
+                <CopyBlock
+                    text={globalErrorExample}
+                    language='python'
+                    showLineNumbers='true'
+                    wrapLines
+                    theme={monokaiSublime}
+                    codeBlock='false'
+                />
+                <p>Once we have this, we can then add if statments for each of the errors we might expect. To get the error value please refer to <Link href="https://nextcord.readthedocs.io/en/latest/ext/commands/api.html#nextcord.ext.commands.CommandError"><a className={styles.docLink} target="_blank"><strong>CommandError</strong></a></Link> for more information. Just for demonstration pouposes we will do three errors together, <code className={CodeStyles.inline}>CommandNotFound</code>, <code className={CodeStyles.inline}>CommandOnCooldown</code>, and <code className={CodeStyles.inline}>MissingPermissions</code>.</p>
+                <CopyBlock
+                    text={commandNotFoundExample}
+                    language='python'
+                    showLineNumbers='true'
+                    wrapLines
+                    theme={monokaiSublime}
+                    codeBlock='false'
+                />
+                <p></p>
+                <CopyBlock
+                    text={commandOnCooldownExample}
+                    language='python'
+                    showLineNumbers='true'
+                    wrapLines
+                    theme={monokaiSublime}
+                    codeBlock='false'
+                />
                 <div className={styles.buttonSwitch}>
                     <Link href="./events">
                         <button className={styles.nextButton}>
